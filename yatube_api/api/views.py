c39d893a4d6518f19f9a4f8dd3a95fcd3ee4943c
Dmitry Permyakov <taren4ik@mail.ru>
2022-04-24 22:03:50 +1000

    Восстановил init и добавил all() в get_queryset

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -47,7 +47,7 @@ class FollowViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
     search_fields = ('following__username',)
 
     def get_queryset(self):
-        return self.request.user.follower
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
